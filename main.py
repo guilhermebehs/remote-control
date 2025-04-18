996fe91a8d82f2d981381b4abcf65bf4713faeac
Guilherme Behs <guilhermebehs2013@hotmail.com>
2025-04-18 10:42:13 -0300

    feat: detect up, down and enter movements

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+import threading
 import cv2
 import mediapipe as mp
+import time
 from commands_adapter import CommandsAdapter
 
 commandsAdapter = CommandsAdapter()
+
+
+def is_hand_closed(hand_landmarks):
+    # Landmarks for finger tips and pip joints
+    tips_ids = [8, 12, 16, 20]  # Index, Middle, Ring, Pinky tips
+    pip_ids = [6, 10, 14, 18]   # Corresponding PIP joints
+
+    closed_fingers = 0
+
+    for tip_id, pip_id in zip(tips_ids, pip_ids):
+        tip = hand_landmarks.landmark[tip_id]
+        pip = hand_landmarks.landmark[pip_id]
+
+        if tip.y > pip.y:  # Remember: lower y means higher on screen
+            closed_fingers += 1
+
+    return closed_fingers >= 4  # You can tweak this threshold
+
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=1)
@@ -11,8 +31,34 @@ mp_draw = mp.solutions.drawing_utils
 cap = cv2.VideoCapture(0)
 
 prev_x = None
-direction = ''
+prev_y = None
+prev_hands_close = None
 
+threshold_x = 100
+threshold_y = 100
+
+cooldown = {
+    commandsAdapter.left: 1.0,
+    commandsAdapter.right: 1.0,
+    commandsAdapter.up: 1.0,
+    commandsAdapter.down: 1.0,
+    commandsAdapter.enter: 2.0,
+}
+last_trigger_times = {
+    commandsAdapter.left: 0,
+    commandsAdapter.right: 0,
+    commandsAdapter.up: 0,
+    commandsAdapter.down: 0,
+    commandsAdapter.enter: 0,
+
+}
+
+def trigger_command(func):
+    now = time.time()
+    if now - last_trigger_times[func] >= cooldown[func]:
+        last_trigger_times[func] = now
+        threading.Thread(target=func).start()
+      
 while True:
     success, img = cap.read()
     if not success:
@@ -25,27 +71,36 @@ while True:
         for hand_landmarks in results.multi_hand_landmarks:
             mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-            # Get wrist landmark (ID 0)
             wrist = hand_landmarks.landmark[0]
             h, w, _ = img.shape
-            cx = int(wrist.x * w)  # Convert normalized to pixel position
-
-            # Compare to previous frame
-           
-            if prev_x is not None and abs(prev_x - cx) > 50:
-                if cx < prev_x:
-                  #commandsAdapter.right()
-                  print("right")
-                elif cx > prev_x:    
-                #commandsAdapter.left()
-                  print("left")
-
+            cx = int(wrist.x * w)
+            cy = int(wrist.y * h)
             
+            hands_closed = is_hand_closed(hand_landmarks) 
+            if prev_hands_close is not None and prev_hands_close != hands_closed :
+               if hands_closed:
+                 trigger_command(commandsAdapter.enter)
+
+            elif prev_x is not None and abs(prev_x - cx) > threshold_x:
+                if cx < prev_x:
+                    trigger_command(commandsAdapter.right)
+                elif cx > prev_x:
+                    trigger_command(commandsAdapter.left)
+
+            elif prev_y is not None and abs(prev_y - cy) > threshold_y:
+                if cy < prev_y:
+                    trigger_command(commandsAdapter.up)
+                elif cy > prev_y:
+                    trigger_command(commandsAdapter.down)        
+
             prev_x = cx
+            prev_y = cy
+            prev_hands_close = hands_closed
+
 
     cv2.imshow("Hand Tracking", img)
 
-    if cv2.waitKey(1) & 0xFF == 27:
+    if cv2.waitKey(1) & 0xFF == 27:  # ESC to exit
         break
 
 cap.release()
